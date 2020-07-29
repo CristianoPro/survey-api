80c3e158aacc9d29e7a5b350eeb408863a8143b7
Cristiano Oliveira <cristiano.so@hotmail.com>
2020-07-29 03:09:49 -0300

    refactor: clear database between test

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,6 +11,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
+  })
+
   const makeSut = (): AddAccountRepository => {
     return new AccountMongoRepository()
   }
