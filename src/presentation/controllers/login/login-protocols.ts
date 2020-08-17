e8b62739ab458c4033840197a730a2bd5d12196c
Cristiano S Oliveira <cristiano.so@hotmail.com>
2020-08-16 21:38:23 -0300

    refactor: group LoginController dependencies in a sigle file

---
+++ src/presentation/controllers/login/login-protocols.ts
@@ -0,0 +1,3 @@
+export { Controller, HttpRequest, HttpResponse } from '../../protocols'
+export { EmailValidator } from '../../protocols/email-validator'
+export { Authentication } from '../../../domain/usecases/authentication'
